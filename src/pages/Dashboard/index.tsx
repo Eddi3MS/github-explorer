a29530f6d16c9e571cab1752066a671b0ea3e44a
Eddi3MS <edsonmarc2014@gmail.com>
2021-02-16 20:32:37 -0300

    teste

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/75024157?s=460&u=b5a2f994a17189d9fa725b091fef45b4ccc83a69&v=4"
+            alt="Eddi3 MS"
+          />
+          <div>
+            <strong>Eddi3 MS</strong>
+            <p>Starting in the web development world.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/75024157?s=460&u=b5a2f994a17189d9fa725b091fef45b4ccc83a69&v=4"
+            alt="Eddi3 MS"
+          />
+          <div>
+            <strong>Eddi3 MS</strong>
+            <p>Starting in the web development world.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/75024157?s=460&u=b5a2f994a17189d9fa725b091fef45b4ccc83a69&v=4"
+            alt="Eddi3 MS"
+          />
+          <div>
+            <strong>Eddi3 MS</strong>
+            <p>Starting in the web development world.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
